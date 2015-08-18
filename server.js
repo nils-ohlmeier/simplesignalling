1f35dff39f3cd6988eb3a290e261f67e6f69a74b
Rithesh Shenthar <rithesh.shenthar@gmail.com>
2015-08-18 13:59:20 -0700

    Added ability to send room information inside message to client
    Added ability to send room information inside message to client

--- server.js
+++ server.js
@@ -10,16 +10,16 @@ io.sockets.on('connection', function (socket) {
     socket.emit("numclients",{'clients': io.sockets.clients(room).length});
     socket.broadcast.to(room).emit("client_joined");
   });
+  socket.on('message_to_server', function (data) {
+    data_json = JSON.parse(data);
+    socket.broadcast.to(data_json.room).send(JSON.stringify({room:data_json.room, msg: JSON.parse(data_json.msg)}));
+  });
 
   socket.on('unsubscribe', function(room) {
     console.log('leaving room', room);
     socket.leave(room); 
     socket.emit("numclients",{'clients': io.sockets.clients(room).length});
     socket.emit("client_exited");
-  });
-
-  socket.on('message', function (data) {
-    socket.broadcast.to(data.room).send(data.msg);
   });
 
   socket.on('disconnect', function () {
